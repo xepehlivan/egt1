16dc7ec2ae8f0dd15255984bd01d6af6c866e175
xepehlivan <pehlivan.ertugruls@gmail.com>
2021-05-22 03:49:45 +0000

    first commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox"
+], function (Controller,MessageBox) {
+	"use strict";
+
+	return Controller.extend("renova.egt1.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"renova/egt1/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
